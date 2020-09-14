42f93dc9cfab84dd483850659fb3164f203bfa00
Travis Lee <travisslee223@gmail.com>
2020-09-14 17:47:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,7 +39,7 @@ def monopoly_with_fourth_tier
   	      :reading_railroad => {},
   	      :pennsylvania_railroad => {},
   	      :b_and_o_railroad => {},
-  	      :reading_railroad => {},
+  	      :shortline => {},
   	    },
   	    :rent_in_dollars => {
   	      :one_piece_owned => 25,
